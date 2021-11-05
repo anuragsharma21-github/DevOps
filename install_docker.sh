532b79edd429427ea8db81d0531f5605176687a0
anuragsharma21-github <58751324+anuragsharma21-github@users.noreply.github.com>
2021-11-05 22:43:34 +0100

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,15 +1,25 @@
 set -x
 # Uninstall Docker if any previous installation exist #
 apt-get remove docker docker-engine docker.io containerd runc
+
 # remove key file if exist from previous installations #
 rm -rf /usr/share/keyrings/docker-archive-keyring.gpg
-# Update the apt package index and install packages to allow apt to use a repository over HTTPS #
+
+# Update the apt package to allow to use a repository over HTTPS #
 apt-get update
-# Add Docker’s official GPG key #
 apt-get install -y  apt-transport-https ca-certificates curl gnupg lsb-release
+
+# Add Docker’s official GPG key #
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Set up the stable repository #
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker Engine #
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Check docker service and Swarm #
+
 docker ps
 docker service ls
